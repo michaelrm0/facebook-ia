1ed5849a306081a965fe88e3ef79d5145d2fe88f
michael <michaelrm0@outlook.com>
2020-10-19 00:47:40 -0500

    Quitando la configuración de os.environ.get

--- app.py
+++ app.py
@@ -4,8 +4,28 @@ from bot import Bot
 import json
 import os
 
-PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
+#PAGE_ACCESS_TOKEN = os.environ.get('PAGE_ACCESS_TOKEN')
 #PAGE_ACCESS_TOKEN = 'EAAGJKF6IdgcBAPvMCTGPZA7OlvmgqMHEj1rjkgSBXkZB1u2wl0iuZB2ROK76jlQFdBxQDw4OKjT6yaZA9MJQCZBaC2mZABSydKcM42YR1AuVh9DZAMmOTA7sEUOKXZARQbNXR2ZBd1iO4mZAAzACZAEbx4Jkx2nENjCh3l99Po35a3HF8jCqZBSKTqmB'
+#PAGE_ACCESS_TOKEN = os.getenv('PAGE_ACCESS_TOKEN')
+
+def read_token():
+    with open("token.txt", "r") as token:
+        lineas = token.readlines()
+        return lineas[0].strip()
+
+def read_verify_token():
+    with open("token_verificador.txt", "r") as verify_token:
+        lineas = verify_token.readlines()
+        return lineas[0].strip()
+
+def read_mode():
+    with open("mode.txt", "r") as mode:
+        lineas = mode.readlines()
+        return lineas[0].strip()
+
+PAGE_ACCESS_TOKEN = read_token()
+VERIFY_TOKEN = read_verify_token()
+MODE = read_mode()
 
 GREETINGS = ['hola', 'cómo estás', 'buenas']
 
@@ -14,15 +34,21 @@ app = Flask(__name__)
 #Creamos la ruta
 @app.route('/', methods = ['GET', 'POST'])
 def webhook():
+    global PAGE_ACCESS_TOKEN
+    global VERIFY_TOKEN
+    global MODE
     if request.method == 'GET':
         '''
         Necesitamos validar el token para que pueda haber un puente de
         enlace con messenger
         '''
-        #VERIFY_TOKEN = 'HolaMundoCruel'
+        print(f"GET: {PAGE_ACCESS_TOKEN} - {VERIFY_TOKEN} - {MODE}")
+        #VERIFY_TOKEN = 'HolaMundoCruel' 
+        
         #MODE = 'subscribe'
-        VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
-        MODE = os.environ.get('MODE')
+        
+        #VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN')
+        #MODE = os.environ.get('MODE')        
 
         token = request.args.get('hub.verify_token')
         challenge = request.args.get('hub.challenge')
@@ -35,8 +61,9 @@ def webhook():
             print(mode)
             return str(challenge)            
         #devolvemos 200 en caso sea exitoso
-        return '200'
-    elif request.method == 'POST':        
+        return '400'
+    elif request.method == 'POST': 
+        print(f"POST: {PAGE_ACCESS_TOKEN} - {VERIFY_TOKEN} - {MODE}")       
         #Guardamos la información de fb en un json        
         print(request.data)
         data = json.loads(request.data)
